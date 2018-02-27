52437d9d13f82aa29d422ecbc9e9f9b7da6ddb81
tazro inutano ohta <inutano@gmail.com>
2018-02-27 16:09:02 +0900

    fixed file extension

--- R/corrplot.R
+++ R/corrplot.R
@@ -74,7 +74,7 @@ x <- pforeach(i = 1:NROW(tfs.vec)) ({
   tf <- tfs.vec[i]
 
   # Create or load matrix from RDS
-  tf.rds.file <- file.path(tfs.rds.dir, paste(tf, "rdfs", sep="."))
+  tf.rds.file <- file.path(tfs.rds.dir, paste(tf, "rds", sep="."))
   if(!file.exists(tf.rds.file)) {
     print(paste("Data for", tf, "is missing in", tfs.rds.dir))
   } else {
